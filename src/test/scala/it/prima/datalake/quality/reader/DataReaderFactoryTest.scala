5413ee3dbcf394a345a87b250da55306fba30c37
dsnicola <nicoladeseta@hotmail.it>
2023-01-30 13:01:12 +0100

    Implemented factory tests

--- src/test/scala/it/prima/datalake/quality/reader/DataReaderFactoryTest.scala
+++ src/test/scala/it/prima/datalake/quality/reader/DataReaderFactoryTest.scala
@@ -1,8 +1,14 @@
 package it.prima.datalake.quality.reader
 
 import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.must.Matchers.{a, convertToAnyMustWrapper}
 
 class DataReaderFactoryTest extends AnyFunSuite {
 
-  test("DataReaderImpl is returned") {} //TODO
+  test("DataReaderImpl is returned") {
+    val dataReader = DataReaderFactory.createDataReader()(null)
+
+    dataReader mustBe a[DataReaderImpl]
+  }
+
 }
--- src/test/scala/it/prima/datalake/quality/writer/ResultsWriterFactoryTest.scala
+++ src/test/scala/it/prima/datalake/quality/writer/ResultsWriterFactoryTest.scala
@@ -1,8 +1,13 @@
 package it.prima.datalake.quality.writer
 
 import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.must.Matchers.{a, convertToAnyMustWrapper}
 
 class ResultsWriterFactoryTest extends AnyFunSuite {
 
-  test("ResultWriterImpl is correctly created") {}//TODO
+  test("ResultWriterImpl is correctly created") {
+    val resultsWriter = ResultsWriterFactory.createResultsWriter(null, null)
+
+    resultsWriter mustBe a[ResultsWriterImpl]
+  }
 }
