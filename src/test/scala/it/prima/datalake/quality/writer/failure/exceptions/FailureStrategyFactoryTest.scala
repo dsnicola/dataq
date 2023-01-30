93dffbac89c62cdbb4c673aaf3c3cbc4f6cede7d
dsnicola <nicoladeseta@hotmail.it>
2023-01-30 17:44:52 +0100

    Implemented failure strategy factory tests

--- src/test/scala/it/prima/datalake/quality/writer/failure/exceptions/FailureStrategyFactoryTest.scala
+++ src/test/scala/it/prima/datalake/quality/writer/failure/exceptions/FailureStrategyFactoryTest.scala
@@ -1,9 +1,26 @@
 package it.prima.datalake.quality.writer.failure.exceptions
 
+import it.prima.datalake.quality.writer.failure.{FailFastStrategy, IgnoreMalformedStrategy}
 import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.must.Matchers.{a, convertToAnyMustWrapper}
 
 class FailureStrategyFactoryTest extends AnyFunSuite {
 
-  test("correct strategy is being created") {}//TODO
-  test("throws an error when the strategy does not exist") {}//TODO
+  test("Fail Fast strategy is being created") {
+    val strategy = FailureStrategyFactory.createFailureStrategy("FAIL_FAST")
+
+    strategy mustBe a[FailFastStrategy]
+  }
+
+  test("Ignore Malformed strategy is being created") {
+    val strategy = FailureStrategyFactory.createFailureStrategy("IGNORE_MALFORMED")
+
+    strategy mustBe a[IgnoreMalformedStrategy]
+  }
+
+  test("throws an error when the strategy does not exist") {
+    assertThrows[IllegalArgumentException] {
+      FailureStrategyFactory.createFailureStrategy("casual input")
+    }
+  }
 }
