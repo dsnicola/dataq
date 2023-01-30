5f525a5bbe631818785e6cff750d480864c8bc52
dsnicola <nicoladeseta@hotmail.it>
2023-01-30 17:33:24 +0100

    Implemented reader tests

--- src/test/scala/it/prima/datalake/quality/reader/DataReaderImplTest.scala
+++ src/test/scala/it/prima/datalake/quality/reader/DataReaderImplTest.scala
@@ -1,8 +1,21 @@
 package it.prima.datalake.quality.reader
 
+import it.prima.datalake.quality.utils.TestUtils
+import org.apache.spark.sql.SparkSession
 import org.scalatest.funsuite.AnyFunSuite
 
 class DataReaderImplTest extends AnyFunSuite {
 
-  test("delta table is returned as dataframe") {} //TODO
+  private implicit val spark: SparkSession = SparkSession.builder().master("local[*]").getOrCreate()
+  spark.sparkContext.setLogLevel("ERROR")
+
+  test("delta table is returned as dataframe") {
+    val dataReader = new DataReaderImpl()
+
+    val location = "./src/test/resources/sample"
+    val actual = dataReader.read(location)
+    val expected = TestUtils.getSampleDataFrame(spark)
+
+    assert(actual.collectAsList() === expected.collectAsList())
+  }
 }
